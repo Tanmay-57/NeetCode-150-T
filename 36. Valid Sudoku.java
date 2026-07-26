07d5a1d69a00badeff0d9d6d21cfd52cc885853a
Tanmay Shidhani <111128934+Tanmay-57@users.noreply.github.com>
2026-07-26 17:07:49 +0530

    Implement Sudoku validation logic in Java

---
+++ 36. Valid Sudoku.java
@@ -0,0 +1,35 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<String, Set<Character>> sq = new HashMap<>();
+
+        for(int r=0; r<9; r++){
+            for(int c=0; c<9; c++){
+                if(board[r][c]=='.') continue;
+
+                String sqKey = (r/3)+","+(c/3);
+
+                if (
+                    rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                    cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                    sq.computeIfAbsent(sqKey, k -> new HashSet<>()).contains(board[r][c])
+                ) {
+                    return false;
+                }
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                sq.get(sqKey).add(board[r][c]);
+
+            }
+        }
+
+        return true;
+    }
+}
